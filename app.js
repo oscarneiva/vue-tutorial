9cafb0d32f76d1cd1f21a3d2678b5f69924c780d
oscar-neiva <oscarneivaeneto@gmail.com>
2017-10-10 18:25:30 -0300

    lesson05

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 new Vue({
     el:'#vue-app',
     data:{
-        name: 'Shaun',
-        job: 'ninja',
-        website: 'lalaa',
-        websiteTag: '<a href="lalalala"> The net ninja website' 
+        age: 25,
+        x: 0,
+        y: 0
     },
     methods:{
-        greet: function(time){
-            return 'Good' + time + this.name;
+        add: function(inc){
+            this.age += inc;
+        },
+        subtract: function(dec){
+            this.age -= dec;
+        },
+        updateXY: function(event){
+            this.x = event.offsetX;
+            this.y = event.offsetY;
         }
     }
 });
